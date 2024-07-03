358c5fd8dfd13b3ab2ef515ddd995a9a7acfafac
m1shabanian <mohamad1shabanian@gmail.com>
2024-07-03 22:10:21 +0330

    home page view backend

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import User, Admin, Product, Order, OrdersProduct, UsersOrders, Storage
+from .models import User, Admin, Product, Order, OrdersProduct, UsersOrders, Storage, Category
 
 admin.site.register(User)
 admin.site.register(Admin)
 admin.site.register(Product)
+admin.site.register(Category)
 admin.site.register(Order)
 admin.site.register(OrdersProduct)
 admin.site.register(UsersOrders)
 admin.site.register(Storage)
-
